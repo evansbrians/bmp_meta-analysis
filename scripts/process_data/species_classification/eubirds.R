3ebb2832eb948793563f17d2d576c22de66b4c75
Brian Evans <evansbr@si.edu>
2026-08-15 10:14:54 -0400

    processing script for eubirds

--- scripts/process_data/species_classification/eubirds.R
+++ scripts/process_data/species_classification/eubirds.R
@@ -1,32 +1,58 @@
 
-# Script for processing eu birds.
-
-# setup -------------------------------------------------------------------
-
-library(traitdata)
+library(here)
 library(tidyverse)
 
-data("eubirds")
+# Grab and format Bird Life International list of species and common names:
 
-eubirds_habitat <-
-  eubirds %>% 
-  janitor::clean_names() %>%  
-  select(
-    species = scientific_name_std, 
-    deciduous_forest:human_settlements
+taxonomy <-
+  here(
+    "data/raw/for_species_classification", 
+    "birdlife_international_all_species.csv"
   ) %>% 
+  read_csv() %>% 
+  janitor::clean_names() %>% 
+  mutate(
+    common_name = 
+      common_name %>% 
+      str_to_lower() %>% 
+      str_replace_all("-", "_") %>% 
+      str_remove_all("'") %>% 
+      str_replace_all(" ", "_"),
+    scientific_name,
+    .keep = "none"
+  )
+
+# Grab eubirds record (Storchova and Horak 2018) from the traitdata package:
+
+eu_birds <- 
+  traitdata::eubirds %>% 
+  janitor::clean_names() %>%
+  as_tibble() %>% 
+  select(scientific_name_std, deciduous_forest:human_settlements) %>% 
+  
+  # Make longform:
+  
   pivot_longer(
-    deciduous_forest:human_settlements,
-    names_to = "habitat_type"
+    cols = deciduous_forest:human_settlements,
+    names_to = "classification"
   ) %>% 
-  filter(
-    value == 1
-  ) %>% 
+  
+  # Subset to classes where the bird occurs:
+  
+  filter(value == 1) %>% 
+  
+  # Flatten to combined classes where the bird occurs:
+  
   summarize(
-    habitat = 
-      str_flatten(
-        habitat_type,
-        collapse = "; "
-      ),
-    .by = species
+    classification = 
+      str_flatten(classification, collapse = "; "),
+    .by = scientific_name_std
+  )
+
+# Add common names:
+  
+eu_birds %>% 
+  anti_join(
+    taxonomy,
+    by = join_by(scientific_name_std == scientific_name)
   )
